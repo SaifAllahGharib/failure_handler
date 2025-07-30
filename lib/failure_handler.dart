3edaadca61aed889282732c16cfba665bb87eaf5
SaifAllahGharib <saifgharib28@gmail.com>
2025-07-30 13:30:24 +0300

    add exports error logger

--- lib/failure_handler.dart
+++ lib/failure_handler.dart
@@ -2,6 +2,7 @@ library failure_handler;
 
 export 'src/di/failure_handler_injection.dart';
 export 'src/error_handler.dart';
+export 'src/error_logger.dart';
 export 'src/mappers/default_error_mapper.dart';
 export 'src/mappers/dio_error_mapper.dart';
 export 'src/mappers/firebase_error_mapper.dart';
