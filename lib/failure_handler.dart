13670813684535684ec6bc293d18085ff41b27ca
SaifAllahGharib <saifgharib28@gmail.com>
2025-07-30 13:21:19 +0300

    add exports mappers

--- lib/failure_handler.dart
+++ lib/failure_handler.dart
@@ -2,6 +2,10 @@ library failure_handler;
 
 export 'src/di/failure_handler_injection.dart';
 export 'src/error_handler.dart';
+export 'src/mappers/default_error_mapper.dart';
+export 'src/mappers/dio_error_mapper.dart';
+export 'src/mappers/firebase_error_mapper.dart';
+export 'src/mappers/supabase_error_mapper.dart';
 export 'src/models/app_failure.dart';
 export 'src/models/result.dart';
 export 'src/types/future_result.dart';
