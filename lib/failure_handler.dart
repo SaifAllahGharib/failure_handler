85f42c11de6adc14da0a95c5e770cabfa9d4edd6
SaifAllahGharib <saifgharib28@gmail.com>
2025-07-30 11:34:40 +0300

    add exports future result and stream result

--- lib/failure_handler.dart
+++ lib/failure_handler.dart
@@ -4,3 +4,5 @@ export 'src/di/failure_handler_injection.dart';
 export 'src/error_handler.dart';
 export 'src/models/app_failure.dart';
 export 'src/models/result.dart';
+export 'src/types/future_result.dart';
+export 'src/types/stream_result.dart';
